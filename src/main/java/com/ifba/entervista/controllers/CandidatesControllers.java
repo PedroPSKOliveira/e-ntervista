e97f0d3b097a58355f1bbd249c408ba4ae356db1
luisaraujoc <luislk_@outlook.com>
2022-10-08 11:28:20 -0300

    restart logical

--- src/main/java/com/ifba/entervista/controllers/CandidatesControllers.java
+++ src/main/java/com/ifba/entervista/controllers/CandidatesControllers.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @RestController
 public class CandidatesControllers {
 
